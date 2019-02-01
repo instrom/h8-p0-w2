062e302ace50e22d0c938737a0ae72a7f073ad46
Tommy Sutjipto <tommysutjipto96@gmail.com>
2019-02-01 14:10:31 +0700

    added exercises friday

---
+++ exercise13_week2.js
@@ -0,0 +1,19 @@
+function xo(str) {
+    var x = 0;
+    var o = 0;
+    for (i=0; i < str.length; i++) {
+        if (str[i] === "x") {
+            x++;
+        } else {
+            o++;
+        }
+    } return x === o
+    // you can only write your code here!
+  }
+  
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
